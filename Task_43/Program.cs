423a964e0b2033435bf8a73c73c12bf13b2f3e56
nikita <tolstoguzov.nikita98@mail.ru>
2023-03-06 14:25:38 +0300

    Задача 43_правки

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -17,33 +17,32 @@ int k2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Ваши уравнения y={k1}x+{b1} и y={k2}x+{b2}");
 
 CheckLine(k1, k2, b1, b2);
-//GetDecision(k1, k2, b1, b2);
 
-bool CheckLine (int koff1, int koff2, int const1, int const2)
+
+void CheckLine (int koff1, int koff2, int const1, int const2)
 {
     if(koff1 == koff2)
     {
         if(const1 == const2)
         {
             Console.WriteLine("Прямые совпадают");
-            return false;
         }
         else
         {
             Console.WriteLine("Прямые паралельны");
-            return false;
         }
 
     }
     else
     GetDecision(koff1, koff2, const1, const2);
-    return true;
 }
 
 
 void GetDecision(int koff1, int koff2, int const1, int const2)
 {
-    double x = (const2 - const1)/(koff1 - koff2);
+    double Const = const2 - const1;
+    double koff = koff1 - koff2;
+    double x = Math.Round(Const, 2) / Math.Round(koff, 2);
     double y = koff1 * x + const1;
     Console.WriteLine($"Ваши отрезки пересекаются в точке: ({x};{y})");
 }
